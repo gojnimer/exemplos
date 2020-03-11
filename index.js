38b215a9ea1a462d29263ce823dc79820b9886f2
Guilherme Nimer Gojlevicius <rm85924@fiap.com.br>
2020-03-10 22:34:29 -0300

    primeiro envio

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function validar(){
+   var nome = document.getElementById("idNome");
+
+    if (nome.value == ""){
+        alert("Preencha o campo Nome");
+        nome.focus();
+        return false;
+    }
+
+
+    if (document.getElementById("idPontos").value == ""){
+        alert("Preencha o campo Pontos");
+        formuser.pontos.focus();
+        return false;
+    }
+
+
+    if (document.getElementById("idNota").value == ""){
+        alert("Preencha o campo Nota");
+        formuser.nota.focus();
+        return false;
+    }
+
+
+    
+    if (document.getElementById("idUni") == ""){
+        alert("Preencha o campo Universidade");
+        formuser.universidade.focus();
+    }
+
+    return false;
+}
